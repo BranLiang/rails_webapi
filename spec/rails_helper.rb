3f124af2f2d19e9faafb4c965830c1db6d8dde57
BranLiang <lby89757@hotmail.com>
2017-03-18 11:57:19 +0800

    setup testing environment

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,7 +30,7 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -59,7 +59,7 @@ RSpec.configure do |config|
 
   # Load factory girl method
   config.include FactoryGirl::Syntax::Methods
-  config.before(:suit) do
+  config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
   end
