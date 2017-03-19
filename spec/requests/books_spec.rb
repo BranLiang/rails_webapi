03a0744f8fa8dcdc7ab7a717b51ddd08219da922
BranLiang <lby89757@hotmail.com>
2017-03-19 11:51:46 +0800

    add filtering builder

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -102,5 +102,56 @@ RSpec.describe 'Books', type: :request do
         end
       end
     end # End of describe 'pagination'
+
+    describe 'sorting' do
+      context 'with valid column name' do
+        it 'sorts the books by "id desc"' do
+          get('/api/books?sort=id&dir=desc')
+          expect(json_body['data'].first['id']).to eq agile_web_dev.id
+          expect(json_body['data'].last['id']).to eq ruby_microscope.id
+        end
+      end
+
+      context 'with invalid column name "fid"' do
+        before { get '/api/books?sort=fid&dir=asc' }
+        it 'gets "400 Bad Request" back' do
+          expect(response.status).to eq 400
+        end
+
+        it 'receives an error' do
+          expect(json_body['error']).to_not be nil
+        end
+
+        it 'receives "sort=fid" as an invalid param' do
+          expect(json_body['error']['invalid_params']).to eq 'sort=fid'
+        end
+      end
+    end # End of 'sorting' describe
+
+    describe 'filtering' do
+      context 'with valid filtering param "q[title_cont]=Microscope"' do
+        it 'receives "Ruby under a microscope" back' do
+          get('/api/books?q[title_cont]=Microscope')
+          expect(json_body['data'].first['id']).to eq ruby_microscope.id
+          expect(json_body['data'].size).to eq 1
+        end
+      end
+
+      context 'with invalid filtering param "q[ftitle_cont]=Microscope"' do
+        before { get('/api/books?q[ftitle_cont]=Ruby') }
+
+        it 'gets "400 Bad request"' do
+          expect(response.status).to eq 400
+        end
+
+        it 'receives an error' do
+          expect(json_body['error']).to_not be nil
+        end
+
+        it 'receives "q[ftitle_cont]=Ruby" as an invalid param' do
+          expect(json_body['error']['invalid_params']).to eq 'a[ftitle_cont]=Ruby'
+        end
+      end
+    end # End of filtering describe
   end
 end
